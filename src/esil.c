9274dc7f63963cdac122d72a6f3d61936802b220
Quim <quim@airmail.cc>
2017-06-06 22:51:21 +0200

    Remove functions not needed anymore.

--- src/esil.c
+++ src/esil.c
@@ -40,47 +40,6 @@ void split_tokens(char* string, i32 max_len) {
   }
 }
 
-char** tokens_from_string(char* string, i32 length, i32 *tokens_length) {
-  i32 count = 1;
-  for(i32 i=0; i<length; i++) {
-    if (string[i] == ESIL_DELIMITER) count++;
-  }
-
-  char** tokens = calloc(count, sizeof(char*));
-  if (tokens == NULL) {
-    perror("Error allocation memory");
-    exit(1);
-  }
-
-  i32 tokens_counter = 0;
-  i32 from = 0;
-  for (i32 i=0; i<length && string[i] != 0; i++) {
-    if (string[i] == ESIL_DELIMITER) {
-      string[i] = 0;
-      if (i != 0) {
-        if (string[i-1] != 0) {
-          tokens[tokens_counter] = (string + from);
-          tokens_counter++;
-        }
-      }
-      from = i + 1;
-    }
-  }
-
-  if (string[length - 1] != 0) {
-    tokens[tokens_counter] = (string + from);
-    tokens_counter++;
-  }
-
-  
-  *tokens_length = tokens_counter;
-  return tokens;
-}
-
-void free_tokens(char** tokens) {
-  free(tokens);
-}
-
 esil_op get_operator(char* const str) {
   for (i32 i=0; i<NUM_ESIL_OPCODES; i++) {
     if (strncmp(esil_op_str[i], str, MAX_ESIL_OP_LEN) == 0) {
